5ba81daff1f95c1fcbf7fa1d40d379f6ecad2b85
longsutls <longsutls@gmail.com>
2018-08-13 21:04:24 +0700

    in ra tiền gốc phải trả hàng tháng

--- src/LoanInterest.java
+++ src/LoanInterest.java
@@ -2,4 +2,14 @@ public class LoanInterest {
     public static double caculateInterest(double loans, double interest_rate, double range) {
         return loans * ((interest_rate + range) / 12);
     }
+
+    public static void main(String[] args) {
+        double loans = 6000;
+        double interest_rate = 0.075;
+        double months = 60;
+        double range;
+
+        double capital = loans / months;
+        System.out.println("Principal pay in " + months + " months: " + capital);
+    }
 }
